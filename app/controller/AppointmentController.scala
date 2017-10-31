d67ffa9dfcae645676c5536fe743477a8954e3a4
Keerath Jaggi <keerath.jaggi@gmail.com>
2017-11-01 02:03:51 +0530

    added functionality to book appointment and manage them

--- app/controller/AppointmentController.scala
+++ app/controller/AppointmentController.scala
@@ -1,15 +1,13 @@
 package controller
 
-import model.{DoctorTable, Doctors}
-import play.api.libs.json.{JsArray, JsObject, JsString, Json}
+import model.{Appointment, AppointmentDetails, AppointmentTable, Appointments, DoctorTable, Doctors, Patient}
+import play.api.libs.json.{JsArray, JsNumber, JsObject, JsString, Json}
 import play.api.mvc.{Action, Controller}
 
-/**
-  * Created by keerath on 31/10/17.
-  */
+
 class AppointmentController extends Controller {
 
-  def listDoctorSpecialities() =  Action { request =>
+  def listDoctorSpecialities() = Action { request =>
     val specialities = Doctors.listSpecialities()
     Ok(JsObject(Seq("specialities" -> JsArray(specialities.map(JsString)))).toString())
   }
@@ -20,4 +18,24 @@ class AppointmentController extends Controller {
     Ok(JsObject(Seq("doctors" -> Json.toJson(filteredDoctors))))
   }
 
+  def bookAppointment = Action { request =>
+    request.body.asJson.map { json =>
+      implicit val reads = Json.reads[Appointment]
+      val appointment = json.as[Appointment]
+      implicit val writes = Json.writes[Appointment]
+      Ok(Json.toJson(Appointments.add(appointment)).toString())
+    }.getOrElse(BadRequest)
+  }
+
+  def listAppointments(doctorId: Long) = Action { request =>
+    val appointmentList = Appointments.list(doctorId.toLong)
+    implicit val writes = Json.writes[AppointmentTable#TableElementType]
+    Ok(Json.toJson(appointmentList).toString())
+  }
+
+  def getDetails(appointmentId: Long) = Action { request =>
+    val details = Appointments.get(appointmentId)
+    implicit val writes = Json.writes[AppointmentDetails]
+    Ok(Json.toJson(details).toString())
+  }
 }
---
+++ app/model/Appointments.scala
@@ -0,0 +1,64 @@
+package model
+
+import play.api.Play
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+import slick.driver.MySQLDriver.api._
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.Try
+
+
+case class Appointment(id: Long, patientId: Long, doctorId: Long, timestamp: Long)
+
+case class AppointmentDetails(appointmentId: Long, appointmentTimestamp: Long,
+                              name: String, gender: String, age: Int, email: String,
+                              emergencyNumber: String, bloodGroup: String)
+
+class AppointmentTable(tag: Tag) extends Table[Appointment](tag, "Appointment") {
+
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+  def patientId = column[Long]("patient_id")
+
+  def patient = foreignKey("patient_fk", patientId, Patients.patientTable)(_.id)
+
+  def doctorId = column[Long]("doctor_id")
+
+  def doctor = foreignKey("doctor_fk", doctorId, Doctors.doctorTable)(_.id)
+
+  def timestamp = column[Long]("timestamp")
+
+  override def * = (id, patientId, doctorId, timestamp) <> ((Appointment.apply _).tupled, Appointment.unapply)
+}
+
+object Appointments {
+
+  val db = DatabaseConfigProvider.get[JdbcProfile](Play.current).db
+  val appointmentTable = TableQuery[AppointmentTable]
+
+  createTableIfNotExist()
+
+  private def createTableIfNotExist(): Unit = {
+    val table = List(appointmentTable)
+    val tableCreationFuture = db.run(DBIO.sequence(table.map(_.schema.create)))
+    Try(Await.result(tableCreationFuture, Duration.Inf))
+  }
+
+  def add(appointment: Appointment) =
+    Await.result(db.run(appointmentTable += appointment).map(res => appointment), Duration.Inf)
+
+  def list(doctorId: Long) = Await.result(db.run(
+    appointmentTable.filter(_.doctorId === doctorId).sortBy(_.timestamp).result), Duration.Inf)
+
+  val appointmentJoinPatient = for {
+    (appointment, patient) <- appointmentTable join Patients.patientTable on (_.patientId === _.id)
+  } yield (appointment.id, appointment.timestamp, patient.name, patient.gender,
+    patient.age, patient.email, patient.emergencyNumber, patient.bloodGroup)
+
+  def get(appointmentId: Long) = (AppointmentDetails.apply _ ).tupled(
+    Await.result(db.run(appointmentJoinPatient
+      .filter(_._1 === appointmentId).result), Duration.Inf).head)
+}
