90cb42294f5c6abb105478a33e65a8b898357740
Keerath Jaggi <keerath.jaggi@gmail.com>
2017-11-03 00:29:10 +0530

    added doctors.csv and patients.csv for default entries

--- app/model/Doctors.scala
+++ app/model/Doctors.scala
@@ -8,10 +8,11 @@ import slick.driver.MySQLDriver.api._
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.io.Source
 import scala.util.Try
 
-case class Doctor(id: Long, name: String, password: String, email: String, phoneNumber: String,
-                  qualifications: String, speciality: String, hospitalName: String)
+case class Doctor(id: Long, name: String, password: String, email: String, qualifications: String,
+                  speciality: String, phoneNumber: String, hospitalName: String)
 
 class DoctorTable(tag: Tag) extends Table[Doctor](tag, "Doctor") {
 
@@ -31,19 +32,27 @@ class DoctorTable(tag: Tag) extends Table[Doctor](tag, "Doctor") {
 
   def hospitalName = column[String]("hospital_name")
 
-  override def * = (id, name, password, email, phoneNumber, qualifications,
-    speciality, hospitalName) <> ((Doctor.apply _).tupled, Doctor.unapply)
+  override def * = (id, name, password, email, qualifications, speciality,
+    phoneNumber, hospitalName) <> ((Doctor.apply _).tupled, Doctor.unapply)
 }
 
 object Doctors {
 
+  private val CSV_DELIM = """,(?=([^\"]*\"[^\"]*\")*[^\"]*$)"""
   val db = DatabaseConfigProvider.get[JdbcProfile](Play.current).db
-
   val doctorTable = TableQuery[DoctorTable]
+
+  private def parseDoctorsFromCSV =
+    Source.fromFile(getClass.getClassLoader.getResource("doctors.csv").getFile).getLines().toList.tail.map { line =>
+      val Array(id, name, password, email, qualifications, speciality, phoneNumber, hospitalName) = line.split(CSV_DELIM)
+      Doctor(id.toLong, name, password, email, qualifications, speciality, phoneNumber, hospitalName)
+    }
 
   createTableIfNotExist()
 
   def add(doctor: Doctor) = Await.result(db.run(doctorTable += doctor).map(res => doctor), Duration.Inf)
+
+  def addAll(doctors: List[Doctor]) = Await.result(db.run(doctorTable ++= doctors), Duration.Inf)
 
   def authenticate(email: String, password: String): Option[DoctorTable#TableElementType] =
     Await.result(db.run(doctorTable.filter(
@@ -52,7 +61,10 @@ object Doctors {
   private def createTableIfNotExist(): Unit = {
     val table = List(doctorTable)
     val tableCreationFuture = db.run(DBIO.sequence(table.map(_.schema.create)))
-    Try(Await.result(tableCreationFuture, Duration.Inf))
+
+    if (Try(Await.result(tableCreationFuture, Duration.Inf)).isSuccess) {
+      addAll(parseDoctorsFromCSV)
+    }
   }
 
   def listSpecialities() =
--- app/model/Patients.scala
+++ app/model/Patients.scala
@@ -8,10 +8,11 @@ import slick.driver.MySQLDriver.api._
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.io.Source
 import scala.util.Try
 
-case class Patient(id: Long, name: String, gender: String, email: String, password: String,
-                   age: Int, bloodGroup: String, phoneNumber: String, emergencyNumber: String)
+case class Patient(id: Long, name: String, password: String, email: String, age: Int, bloodGroup: String,
+                   phoneNumber: String, emergencyNumber: String, gender: String)
 
 class PatientTable(tag: Tag) extends Table[Patient](tag, "Patient") {
 
@@ -33,27 +34,40 @@ class PatientTable(tag: Tag) extends Table[Patient](tag, "Patient") {
 
   def emergencyNumber = column[String]("emergency_number")
 
-  override def * = (id, name, gender, email, password,
-    age, bloodGroup, phoneNumber, emergencyNumber) <> ((Patient.apply _).tupled, Patient.unapply)
+  override def * = (id, name, password, email, age, bloodGroup,
+    phoneNumber, emergencyNumber, gender) <> ((Patient.apply _).tupled, Patient.unapply)
 }
 
 object Patients {
 
+  private val CSV_DELIM = """,(?=([^\"]*\"[^\"]*\")*[^\"]*$)"""
   val db = DatabaseConfigProvider.get[JdbcProfile](Play.current).db
-
   val patientTable = TableQuery[PatientTable]
 
   createTableIfNotExist()
 
   def add(patient: Patient) = Await.result(db.run(patientTable += patient).map(res => patient), Duration.Inf)
 
+  def addAll(patients: List[Patient]) = Await.result(db.run(patientTable ++= patients), Duration.Inf)
+
   def authenticate(email: String, password: String): Option[PatientTable#TableElementType] =
     Await.result(db.run(patientTable.filter(
       patient => patient.email === email && patient.password === password).result), Duration.Inf).headOption
 
+  private def parsePatientsFromCSV = {
+    Source.fromFile(getClass.getClassLoader.getResource("patients.csv").getFile).getLines().toList.tail.map { line =>
+      val Array(id, name, password, email, age, bloodGroup, phoneNumber, emergencyNumber, gender) = line
+        .split(CSV_DELIM)
+      Patient(id.toLong, name, password, email, age.toInt, bloodGroup, phoneNumber, emergencyNumber, gender)
+    }
+  }
+
   private def createTableIfNotExist(): Unit = {
     val table = List(patientTable)
     val tableCreationFuture = db.run(DBIO.sequence(table.map(_.schema.create)))
-    Try(Await.result(tableCreationFuture, Duration.Inf))
+
+    if (Try(Await.result(tableCreationFuture, Duration.Inf)).isSuccess) {
+      addAll(parsePatientsFromCSV)
+    }
   }
 }
